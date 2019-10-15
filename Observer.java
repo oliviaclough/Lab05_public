d28e31cfe1b86d45c04a9b396b8ab293cbd56a50
oliviaclough <38055262+oliviaclough@users.noreply.github.com>
2019-10-15 15:47:44 -0700

    Create Observer.java

---
+++ Observer.java
@@ -0,0 +1,10 @@
+//Observer.java
+//Olivia Clough 5487343
+//Ryan Mitchell 5761994
+
+public interface Observer {
+    //interface
+
+    public void update (Post post);
+    //Updates the user by simply printing out the posted message. Your console output should state that the displayed post is a result of updating a subscribed user
+}
